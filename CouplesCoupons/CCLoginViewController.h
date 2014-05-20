6c69765cd0ee1b5164e90675c7faeeb1767edeef
Darren Ng <darren.ng@eproximiti.com>
2014-05-19 23:13:46 -0400

    Added login story board and viewcontrollers.

---
+++ CouplesCoupons/CCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCLoginViewController.h
+//  CouplesCoupons
+//
+//  Created by eProximiti 10 on 5/19/14.
+//  Copyright (c) 2014 Aaron.RIetschlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCLoginViewController : UIViewController
+
+@end
---
+++ CouplesCoupons/CCLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  CCLoginViewController.m
+//  CouplesCoupons
+//
+//  Created by eProximiti 10 on 5/19/14.
+//  Copyright (c) 2014 Aaron.RIetschlin. All rights reserved.
+//
+
+#import "CCLoginViewController.h"
+
+@interface CCLoginViewController ()
+
+@end
+
+@implementation CCLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
