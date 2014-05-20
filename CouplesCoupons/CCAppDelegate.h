c0426d7c091d0259c7fd4863675e9f4f13614673
Darren Ng <darren.ng@eproximiti.com>
2014-05-19 22:16:39 -0400

    First commit

---
+++ CouplesCoupons/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  CouplesCoupons
+//
+//  Created by eProximiti 10 on 5/19/14.
+//  Copyright (c) 2014 Aaron.RIetschlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
